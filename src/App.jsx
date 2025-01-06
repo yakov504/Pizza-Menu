4e3083f0ccbe65f40dc32565db9d53e04b20858f
yakov504 <yakov0546080824@gmail.com>
2025-01-06 20:01:45 +0200

    pizza spinaci

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-     
+      <h1>hello world</h1>
+     <Pizza/>
+     <Pizza/>
+     <Pizza/>
     </div>
   );
+}
+function Pizza(){
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="pizza"/>
+      <h2>Pizza Spinaci</h2>
+      <p>tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>    
+  )
 }
 
 export default App;
