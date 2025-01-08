6d8ddae014d8dc299d12339e229e0d176ffbd8dd
yakov504 <yakov0546080824@gmail.com>
2025-01-08 19:56:28 +0200

    some components

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
-     <Pizza/>
-     <Pizza/>
-     <Pizza/>
+     <Header/>
+     <Menu/>
+     <Footer/>
     </div>
   );
+}
+
+function Header(){
+  return(
+    <div>
+      <h1>-FAST REACT PIZZA CO.-</h1>
+    </div>
+  )
+}
+
+function Menu(){
+  return(
+    <div>
+      <h2>Our menu</h2>
+      <Pizza/>
+      <Pizza/>
+      <Pizza/>
+    </div>
+  )
+}
+
+function Footer(){
+
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  // const isOpen = 
+
+  return(
+   <footer>
+    {new Date().toLocaleDateString()}we are currently open
+   </footer>
+  )
 }
 function Pizza(){
   return (
